b71e2c54276b674fee1f2009818f0021c4c46430
Stephanie Hill <stephni.hill@gmail.com>
2023-01-10 18:22:51 -0800

    removed nested comments component

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,6 +1,7 @@
-import NestedComments from './NestedComments';
+
 import { useContext } from "react";
 import { SettingsContext } from '../context/settings'
+
 // form to add comments
 // button to delete comments
 
@@ -27,29 +28,6 @@ const Comments = ({ children }) => {
         </div>
       ))}
     </div>
-    <div>
-          {
-            commentData.map((comment) => {
-              return (
-                <NestedComments key={comment.id} comment={comment} />
-              )
-            })
-          }
-
-    </div>
-      {/* <div>
-        {props.allComments.map((comment, index) => (
-          <div key={`comment-${index}`} className="comment-container">
-            <p>{comment.text}</p>
-            {comment.children.map((childComment, index) => (
-              <div key={`child-${index}`} className="nested-comment-container">
-                <p>{childComment.text}</p>
-                {console.log('comment children ----->', comment.children)}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div> */}
     </>
   )
 }
